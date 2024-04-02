44c8c6043eeb50e0f0d219b49654abaf1d4dfaad
poojithreddy4 <pmadduru@albany.edu>
2024-04-02 11:42:29 -0400

    frontend popup component

--- classconnect/src/components/popup.js
+++ classconnect/src/components/popup.js
@@ -1,6 +1,7 @@
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import { AiOutlineEdit } from "react-icons/ai";
+import Cantpost from './cantpost';
 
 
 function PopUp() {
@@ -13,7 +14,8 @@ function PopUp() {
                             <label className='m-1'>Enter your response:</label>
                             <textarea className="form-control span6" rows="10" columns='100' placeholder="Enter your response" required></textarea>
                             <div className='d-flex justify-content-around m-3'>
-                                <button type="submit" className="btn" style={{ backgroundColor: '#8566A5', color: 'white' }}>Submit</button>
+                                {/* <button type="submit" className="btn" style={{ backgroundColor: '#8566A5', color: 'white' }}>Submit</button> */}
+                                <Cantpost/>
                                 <button onClick={() => { close() }} className="close btn" style={{ backgroundColor: '#FA7B7B', color: 'white' }}>Cancel</button>
 
                             </div>
