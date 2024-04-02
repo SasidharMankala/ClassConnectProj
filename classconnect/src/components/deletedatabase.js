2b3d866fba4f1a2bab062a6723d322617539b6aa
poojithreddy4 <pmadduru@albany.edu>
2024-04-02 11:39:09 -0400

    component for deleting database

--- classconnect/src/components/deletedatabase.js
+++ classconnect/src/components/deletedatabase.js
@@ -1,33 +1,62 @@
-function DeleteDatabase(){
+import { useState } from "react";
 
-    const deletebuttonhandle=()=>{
-        window.location.reload()
+function DeleteClassCom(){
+
+    const [univName, setUnivname] = useState('')
+    const [id, setId] = useState('')
+    const [message, setMessage] = useState('')
+    const [showAlert, setShowAlert] = useState(false); // New state for controlling alert visibility
+
+    const deletebuttonhandle = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await fetch('http://localhost:3001/universities',{
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ univName }),
+            })
+            if(response.status === 200){
+                setMessage('University DELETED successfully');
+                setShowAlert(true); // Show the danger alert
+                setTimeout(() => {
+                    setShowAlert(false); // Hide the alert after 5 seconds
+                }, 5000);
+            } else {
+                const resmes = await response.text()
+                setMessage(resmes)
+            }
+        } catch (error) {
+            console.log('Error deleting University:', error)
+        }
+        setId('')
+        setUnivname('')
     }
-    const cancelbuttonhandle=()=>{
+
+    const cancelbuttonhandle = () => {
         window.location.reload();
     }
 
     return (
         <div className="mt-2 form-group" style={{width:'70%'}}>
             <p className="text-center text-danger fw-bold" >Danger Zone!!</p>
-            <label className='text-danger'htmlFor="">Enter the University you want to delete</label>
+            <label className='text-danger' htmlFor="">Enter the University you want to delete</label>
             <input
                 type="text"
                 className="mt-2 form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
                 placeholder="Enter the University Name"
                 required
+                value={univName}
+                onChange={(e)=>{setUnivname(e.target.value)}}
             />
 
             <label className="text-danger mt-2" htmlFor="">Enter the University ID</label>
             <input
                 type="text"
                 className="mt-2 form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
                 placeholder="Enter the University ID"
                 required
+                value={id}
+                onChange={(e)=>{setId(e.target.value)}}
             />
 
            <div className="d-flex justify-content-around">
@@ -38,8 +67,13 @@ function DeleteDatabase(){
                 Cancel
             </button> 
            </div>
+           {showAlert && ( 
+                <div className="alert alert-danger position-fixed bottom-0 start-50 translate-middle-x" role="alert" style={{ zIndex: 1000 }}>
+                    Database deleted successfully!
+                </div>
+            )}
         </div>
     )
 }
 
-export default DeleteDatabase;
+export default DeleteClassCom;
