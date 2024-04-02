b156d23a2a1f73abcc06fc0e0dc8fbeb97473849
poojithreddy4 <pmadduru@albany.edu>
2024-04-02 11:38:23 -0400

    create database component and home page

--- classconnect/src/components/createdatabase.js
+++ classconnect/src/components/createdatabase.js
@@ -1,23 +1,54 @@
-function CreateDatabase() {
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
-    const createbuttonhandle = ()=>{
-        window.location.reload();
+function CreateDatabase() {
+    const [univName, setUnivName] = useState('');
+    const [univId, setUnivId] = useState('');
+    const [message, setMessage] = useState('');
+    const [showAlert, setShowAlert] = useState(false); // New state for controlling alert visibility
+
+    const handleAddDB = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/universities', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ univName }),
+            });
+            if (response.status === 200) {
+                setMessage('University added successfully');
+                setShowAlert(true); // Show the success alert
+                setTimeout(() => {
+                    setShowAlert(false); // Hide the alert after 2 seconds
+                }, 5000);
+            } else {
+                const resmes = await response.text()
+                setMessage(resmes)
+            }
+        } catch (error) {
+            console.error('Error adding University:', error);
+        }
+        setUnivId('')
+        setUnivName('')
     }
 
-    const cancelbuttonhandle = () =>{
+    const createButtonHandle = () => {
+        handleAddDB();
+    }
+
+    const cancelButtonHandle = () => {
         window.location.reload();
     }
 
     return (
-        <div className="mt-2 form-group" style={{width:'70%'}}>
+        <div className="mt-2 form-group" style={{ width: '70%' }}>
             <p className="text-center fw-bold" >Welcome!!</p>
             <label htmlFor="">Enter the University name</label>
             <input
                 type="text"
                 className="mt-2 form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
                 placeholder="Enter the University Name"
+                value={univName}
+                onChange={(e) => { setUnivName(e.target.value) }}
                 required
             />
 
@@ -25,22 +56,28 @@ function CreateDatabase() {
             <input
                 type="text"
                 className="mt-2 form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
                 placeholder="Enter the University ID"
+                value={univId}
+                onChange={(e) => { setUnivId(e.target.value) }}
                 required
             />
 
-           <div className="d-flex justify-content-around">
-           <button className="btn btn-primary mt-3 w-100" onClick={createbuttonhandle}>
-                Create
-            </button>
-            <button className="btn text-white mt-3 w-100" onClick={cancelbuttonhandle} style={{ backgroundColor: '#FA7B7B' }}>
-                Cancel
-            </button>
-           </div>
+            <div className="d-flex justify-content-around">
+                <button className="btn btn-primary mt-3 w-100" onClick={createButtonHandle}>
+                    Create
+                </button>
+                <button className="btn text-white mt-3 w-100" onClick={cancelButtonHandle} style={{ backgroundColor: '#FA7B7B' }}>
+                    Cancel
+                </button>
+            </div>
+            <p className="mt-3 text-center">{message}</p>
+            {showAlert && ( // Conditionally render the alert
+                <div className="alert alert-success position-fixed bottom-0 start-50 translate-middle-x" role="alert" style={{ zIndex: 1000 }}>
+                    Database for {univName} created successfully
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
 export default CreateDatabase;
--- classconnect/src/pages/home.js
+++ classconnect/src/pages/home.js
@@ -5,13 +5,29 @@ import '../css/home.css'
 import ThreadExpansion from "./thread_expansion";
 import AddNewThread from "../components/addNewThread";
 import Loading from "../components/loading";
+import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 
 function Home() {
-    const [courseId, setCourseId] = useState("course1"); // Default course ID
+    const finalData = []
+    const [courseId, setCourseId] = useState(''); // Default course ID
     const [courseIds, setCourseIds] = useState([]); // Array to store course IDs
-    const [threadData, setThreadData] = useState(null);
+    // const [threadData, setThreadData] = useState([]);
     const [selectedThreadId, setSelectedThreadId] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertemail, setAlertemail] = useState()
+    const [role, setrole] = useState('')
+    const [univName, setUnivName] = useState('')
+    const [retrived, setRetrived] = useState('')
+    const [univData, setUnivData] = useState()
+    const [requiredThreads, setRequiredThreads] = useState([]);
+    const [courseList, setCourseList] = useState([]);
+   
+
+
+    let navigate = useNavigate();
 
     const commentClickhandle = (id) => {
         setSelectedThreadId(id);
@@ -21,58 +37,204 @@ function Home() {
         setSelectedThreadId(null);
     }
 
+    const routtoLogin = () => {
+        let path = `/`;
+        navigate(path);
+    }
+
+    // const fetchThreads = async () => {
+    //     try {
+    //         axios.get('http://localhost:3001/threads')
+    //             .then(response => {
+    //                 const univName = Cookies.get('univname')
+    //                 console.log(univName)
+    //                 // console.log(response.data)
+    //                 const threads = response.data; // Extract the threads data
+
+    //                 // Function to filter threads based on university name
+    //                 const getRequiredThreads = (univName) => {
+    //                     const univData = threads.filter(thread => thread.name === univName)
+    //                     setUnivData(univData)
+    //                     return univData;
+    //                 };
+
+    //                 // Function to extract course list from threads
+    //                 const getCourseList = (requiredThreads) => {
+    //                     const courseSet = new Set(); // Use Set to avoid duplicate course names
+    //                     requiredThreads.forEach(thread => {
+    //                         thread.courses.forEach(course => {
+    //                             courseSet.add({ id: course.id, name: course.name });
+    //                         });
+    //                     });
+    //                     return Array.from(courseSet); // Convert Set back to array
+    //                 };
+
+    //                 const getThreads = (requiredThreads,courseId) => {
+    //                     const threadData = [];
+    //                     console.log(courseId)
+    //                     requiredThreads.forEach(data => {
+    //                         // console.log('data',data)
+    //                         data.courses.forEach(course => {
+    //                             // console.log('data.course',data.courses)
+    //                             if (course.id === courseId.id && course.name === courseId.name) {
+    //                                 course.threads.forEach(thread => {
+    //                                     // console.log('thread',thread)
+    //                                     sneha.push(thread);
+    //                                 });
+    //                             }
+    //                         });
+    //                     }); return Array.from(threadData)
+    //                 }
+
+    //                 const getOnlyCourse = (requiredThreads) => {
+    //                     const onlycourse = []
+    //                     requiredThreads.forEach(thread => {
+    //                         thread.courses.forEach(course => {
+    //                             onlycourse.push(course.name)
+    //                         })
+    //                     })
+    //                     return onlycourse
+    //                 }
+    //                 const requiredThreads = getRequiredThreads(univName);
+    //                 const courseList = getCourseList(requiredThreads);
+    //                 const onlycourse = getOnlyCourse(requiredThreads);
+    //                 const needed = getThreads(requiredThreads,courseId)
+    //                 console.log('needed',needed)
+    //                 setCourseList(courseList)
+    //                 setThreadData(needed)
+    //                 const initial = courseList[0]
+    //                 console.log('requiredThreads', requiredThreads);
+    //                 console.log('courseList', courseList);
+    //                 console.log('onlycourse', onlycourse)
+    //                 console.log('themedata',threadData)
+    //             })
+    //             .catch(error => {
+    //                 console.error('Error fetching data:', error);
+    //             });
+    //     } catch (error) {
+    //         console.error('Error fetching threads:', error);
+    //     }
+    // }
+
+
     useEffect(() => {
+        const email = Cookies.get('email')
+        setAlertemail(email)
+        const role = Cookies.get('role')
+        setrole(role)
+        const univname = Cookies.get('univname')
+        setUnivName(univname)
+
+
+        if (!email) {
+            routtoLogin()
+        }
         setLoading(true); // Set loading to true when changing courses
         // Simulate loading for 1 second
+        // fetchThreads()
         const timeout = setTimeout(() => {
-            console.log('werty')
-             // After 1 second, set loading to false
-        
+            axios.get('http://localhost:3001/threads')
+                .then(response => {
+                    const univName = Cookies.get('univname')
+                    // console.log(univName)
+                    // console.log(response.data)
+                    const threads = response.data; // Extract the threads data
 
-        // Fetch data from JSON file
-        fetch('./updatesthreads.json')
-            .then(response => response.json())
-            .then(data => {
-                // Extract course IDs from data
-                const ids = data.courses.map(course => course.course_id);
-                setCourseIds(ids); // Update courseIds state
-                // Set thread data based on selected course ID
-                const selectedCourse = data.courses.find(course => course.course_id === courseId);
-                if (selectedCourse) {
-                    setThreadData(selectedCourse.threads);
-                } else {
-                    setThreadData(null); // Set threadData to null if course is not found
-                }
-                setLoading(false); // Set loading to false after data is fetched
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-                setLoading(false); // Handle loading state in case of error
-            });}, 1000);
+                    // Function to filter threads based on university name
+                    const getRequiredThreads = (univName) => {
+                        const univData = threads.filter(thread => thread.name === univName)
+                        setUnivData(univData)
+                        return univData;
+                    };
+
+                    // Function to extract course list from threads
+                    const getCourseList = (requiredThreads) => {
+                        const courseSet = new Set(); // Use Set to avoid duplicate course names
+                        requiredThreads.forEach(thread => {
+                            thread.courses.forEach(course => {
+                                courseSet.add({ id: course.id, name: course.name });
+                            });
+                        });
+                        return Array.from(courseSet); // Convert Set back to array
+                    };
+
+                    const getThreads = (requiredThreads, courseId) => {
+                        const threadData = [];
+                        // console.log(courseId)
+                        requiredThreads.forEach(data => {
+                            // console.log('data',data)
+                            data.courses.forEach(course => {
+                                // console.log('data.course',data.courses)
+                                if (course.id === courseId.id && course.name === courseId.name) {
+                                    course.threads.forEach(thread => {
+                                        // console.log('thread',thread)
+                                        finalData.push(thread);
+                                    });
+                                }
+                            });
+                        });
+
+                        return Array.from(threadData)
+                    }
+
+                    const getOnlyCourse = (requiredThreads) => {
+                        const onlycourse = []
+                        requiredThreads.forEach(thread => {
+                            thread.courses.forEach(course => {
+                                onlycourse.push(course.name)
+                            })
+                        })
+                        return onlycourse
+                    }
+                    const requiredThreads = getRequiredThreads(univName);
+                    const courseList = getCourseList(requiredThreads);
+                    const onlycourse = getOnlyCourse(requiredThreads);
+                    const needed = getThreads(requiredThreads, courseId)
+                    // console.log('needed',needed[0])
+                    setCourseList(courseList)
+                    // if(courseId){
+                    //     setThreadData(needed)
+                    // }
+                    const initial = courseList[0]
+                    // console.log('requiredThreads', requiredThreads);
+                    // console.log('courseList', courseList);
+                    // console.log('onlycourse', onlycourse)
+                   
+
+
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+            setLoading(false); // Set loading to false after data is fetched
+            setShowAlert(true); // Show alert after data is fetched
+        }, 1000);
 
         // Cleanup the timeout to prevent memory leaks
         return () => clearTimeout(timeout);
     }, [courseId]); // Update thread data when courseId changes
-
+    const conso = (thread) => {
+        console.log(thread)
+    }
     return (
+
         <>
-            <NavBar courseIds={courseIds} selectedCourse={courseId} onCourseSelect={setCourseId} />
+            <NavBar courseIds={courseList} selectedCourse={courseId} onCourseSelect={setCourseId} />
             {loading && <Loading />}
-            {!loading && threadData && threadData.map((thread, index) => (
-                selectedThreadId === thread._id ?
-                    <ThreadExpansion key={index} threadData={thread} onBack={handleBackToHome} /> :
-                    <ThreadCards
-                        key={index}
-                        id={thread._id}
-                        card_title={thread.thread_title}
-                        card_description={thread.thread_description}
-                        no_replies={thread.no_of_replies}
-                        no_upvotes={thread.no_of_upvotes}
-                        user_name={thread.Username}
-                        onClick={() => commentClickhandle(thread._id)}
-                    />
-            ))}
+            {!loading && finalData &&
+                finalData.map((item, index) => {
+                    <div key={item._id}>
+                        <p>{item.title}</p>
+                    </div>
+                })
+            }
             <AddNewThread />
+            {showAlert && (
+                <div className="alert alert-info alert-dismissible fade show" role="alert" style={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}>
+                    Logged in as {role} {alertemail}
+                    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setShowAlert(false)}></button>
+                </div>
+            )}
         </>
     );
 }
