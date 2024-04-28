fc61374e389b89590ef06df6c7500a1f9bcb63a2
Sasidhar Mankala <60248183+SasidharMankala@users.noreply.github.com>
2024-04-28 17:55:24 -0400

    Add files via upload
    Created basic react app and added required files

--- classconnect/src/pages/home.js
+++ classconnect/src/pages/home.js
@@ -8,6 +8,7 @@ import Loading from "../components/loading";
 import Cookies from 'js-cookie';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
+import ENDPOINT from "../endpoint";
 
 function Home() {
     const finalData = []
@@ -22,16 +23,17 @@ function Home() {
     const [univName, setUnivName] = useState('')
     const [retrived, setRetrived] = useState('')
     const [univData, setUnivData] = useState()
-    const [requiredThreads, setRequiredThreads] = useState([]);
+    const [newton, setnewton] = useState('');
     const [courseList, setCourseList] = useState([]);
-   
+    const [threads, setThreads] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState(null);
 
+    const handleCourseSelection = (course) => {
+        setSelectedCourse(course);
+        // console.log('selectedCourse',course)
+    }
 
     let navigate = useNavigate();
-
-    const commentClickhandle = (id) => {
-        setSelectedThreadId(id);
-    }
 
     const handleBackToHome = () => {
         setSelectedThreadId(null);
@@ -42,80 +44,6 @@ function Home() {
         navigate(path);
     }
 
-    // const fetchThreads = async () => {
-    //     try {
-    //         axios.get('http://localhost:3001/threads')
-    //             .then(response => {
-    //                 const univName = Cookies.get('univname')
-    //                 console.log(univName)
-    //                 // console.log(response.data)
-    //                 const threads = response.data; // Extract the threads data
-
-    //                 // Function to filter threads based on university name
-    //                 const getRequiredThreads = (univName) => {
-    //                     const univData = threads.filter(thread => thread.name === univName)
-    //                     setUnivData(univData)
-    //                     return univData;
-    //                 };
-
-    //                 // Function to extract course list from threads
-    //                 const getCourseList = (requiredThreads) => {
-    //                     const courseSet = new Set(); // Use Set to avoid duplicate course names
-    //                     requiredThreads.forEach(thread => {
-    //                         thread.courses.forEach(course => {
-    //                             courseSet.add({ id: course.id, name: course.name });
-    //                         });
-    //                     });
-    //                     return Array.from(courseSet); // Convert Set back to array
-    //                 };
-
-    //                 const getThreads = (requiredThreads,courseId) => {
-    //                     const threadData = [];
-    //                     console.log(courseId)
-    //                     requiredThreads.forEach(data => {
-    //                         // console.log('data',data)
-    //                         data.courses.forEach(course => {
-    //                             // console.log('data.course',data.courses)
-    //                             if (course.id === courseId.id && course.name === courseId.name) {
-    //                                 course.threads.forEach(thread => {
-    //                                     // console.log('thread',thread)
-    //                                     sneha.push(thread);
-    //                                 });
-    //                             }
-    //                         });
-    //                     }); return Array.from(threadData)
-    //                 }
-
-    //                 const getOnlyCourse = (requiredThreads) => {
-    //                     const onlycourse = []
-    //                     requiredThreads.forEach(thread => {
-    //                         thread.courses.forEach(course => {
-    //                             onlycourse.push(course.name)
-    //                         })
-    //                     })
-    //                     return onlycourse
-    //                 }
-    //                 const requiredThreads = getRequiredThreads(univName);
-    //                 const courseList = getCourseList(requiredThreads);
-    //                 const onlycourse = getOnlyCourse(requiredThreads);
-    //                 const needed = getThreads(requiredThreads,courseId)
-    //                 console.log('needed',needed)
-    //                 setCourseList(courseList)
-    //                 setThreadData(needed)
-    //                 const initial = courseList[0]
-    //                 console.log('requiredThreads', requiredThreads);
-    //                 console.log('courseList', courseList);
-    //                 console.log('onlycourse', onlycourse)
-    //                 console.log('themedata',threadData)
-    //             })
-    //             .catch(error => {
-    //                 console.error('Error fetching data:', error);
-    //             });
-    //     } catch (error) {
-    //         console.error('Error fetching threads:', error);
-    //     }
-    // }
-
 
     useEffect(() => {
         const email = Cookies.get('email')
@@ -124,117 +52,109 @@ function Home() {
         setrole(role)
         const univname = Cookies.get('univname')
         setUnivName(univname)
+        const courses = Cookies.get('courses')
+        setCourseList(courses)
 
+
+        setShowAlert(true);
+
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 2000)
 
         if (!email) {
             routtoLogin()
         }
-        setLoading(true); // Set loading to true when changing courses
-        // Simulate loading for 1 second
-        // fetchThreads()
+        setLoading(true);
         const timeout = setTimeout(() => {
-            axios.get('http://localhost:3001/threads')
-                .then(response => {
-                    const univName = Cookies.get('univname')
-                    // console.log(univName)
-                    // console.log(response.data)
-                    const threads = response.data; // Extract the threads data
+            // console.log('hiiiiii') 
+            const fetchData = async () => {
+                try {
+                    const temp = JSON.parse(courseList);
+                    // console.log('courseList',typeof(temp))
+                    axios.get(`${ENDPOINT}/threads`).then(response => {
+                        // console.log(response.data)
+                        const university = response.data.find(univ => univ.name === univName);
+                        let threadList = [];
 
-                    // Function to filter threads based on university name
-                    const getRequiredThreads = (univName) => {
-                        const univData = threads.filter(thread => thread.name === univName)
-                        setUnivData(univData)
-                        return univData;
-                    };
-
-                    // Function to extract course list from threads
-                    const getCourseList = (requiredThreads) => {
-                        const courseSet = new Set(); // Use Set to avoid duplicate course names
-                        requiredThreads.forEach(thread => {
-                            thread.courses.forEach(course => {
-                                courseSet.add({ id: course.id, name: course.name });
-                            });
-                        });
-                        return Array.from(courseSet); // Convert Set back to array
-                    };
-
-                    const getThreads = (requiredThreads, courseId) => {
-                        const threadData = [];
-                        // console.log(courseId)
-                        requiredThreads.forEach(data => {
-                            // console.log('data',data)
-                            data.courses.forEach(course => {
-                                // console.log('data.course',data.courses)
-                                if (course.id === courseId.id && course.name === courseId.name) {
-                                    course.threads.forEach(thread => {
-                                        // console.log('thread',thread)
-                                        finalData.push(thread);
+                        if (university) {
+                            // Iterate through the courseList
+                            temp.forEach(course => {
+                                // Find the course object in the university's courses array
+                                const courseData = university.courses.find(c => c.id === course.id);
+                                if (courseData) {
+                                    // Push the course data along with its threads into threadList
+                                    threadList.push({
+                                        id: courseData.id,
+                                        name: courseData.name,
+                                        threads: courseData.threads
                                     });
                                 }
+
                             });
-                        });
-
-                        return Array.from(threadData)
-                    }
-
-                    const getOnlyCourse = (requiredThreads) => {
-                        const onlycourse = []
-                        requiredThreads.forEach(thread => {
-                            thread.courses.forEach(course => {
-                                onlycourse.push(course.name)
-                            })
-                        })
-                        return onlycourse
-                    }
-                    const requiredThreads = getRequiredThreads(univName);
-                    const courseList = getCourseList(requiredThreads);
-                    const onlycourse = getOnlyCourse(requiredThreads);
-                    const needed = getThreads(requiredThreads, courseId)
-                    // console.log('needed',needed[0])
-                    setCourseList(courseList)
-                    // if(courseId){
-                    //     setThreadData(needed)
-                    // }
-                    const initial = courseList[0]
-                    // console.log('requiredThreads', requiredThreads);
-                    // console.log('courseList', courseList);
-                    // console.log('onlycourse', onlycourse)
-                   
+                        }
+                        setThreads(threadList)
+                        // console.log('threadList',threadList)
 
 
-                })
-                .catch(error => {
+                    })
+
+                } catch (error) {
                     console.error('Error fetching data:', error);
-                });
+                }
+            }
+            fetchData()
+
             setLoading(false); // Set loading to false after data is fetched
-            setShowAlert(true); // Show alert after data is fetched
+
+            // Show alert after data is fetched
         }, 1000);
 
         // Cleanup the timeout to prevent memory leaks
-        return () => clearTimeout(timeout);
-    }, [courseId]); // Update thread data when courseId changes
-    const conso = (thread) => {
-        console.log(thread)
-    }
-    return (
+        // return () => clearTimeout(timeout); 
+    }, [courseList]); // Update thread data when courseId changes
 
+
+    let selectedCourseThreads = [];
+
+    if (selectedCourse) {
+        // Filter threads based on the selected course's ID
+        selectedCourseThreads = threads.find(course => course.id === selectedCourse.id)?.threads || [];
+        //   console.log('selectedCourseThreads',selectedCourseThreads)/
+    } else {
+        // Render threads of the first course if selectedCourse doesn't exist
+        selectedCourseThreads = threads.length > 0 ? threads[0].threads : [];
+    }
+
+
+    return (
         <>
-            <NavBar courseIds={courseList} selectedCourse={courseId} onCourseSelect={setCourseId} />
+            <NavBar threads={threads} onSelectedCourseChange={handleCourseSelection} />
             {loading && <Loading />}
-            {!loading && finalData &&
-                finalData.map((item, index) => {
-                    <div key={item._id}>
-                        <p>{item.title}</p>
-                    </div>
-                })
-            }
-            <AddNewThread />
+            {!loading && selectedCourseThreads.length !== 0 && selectedCourseThreads.map((thread) => (
+                <ThreadCards
+                    key={thread._id}
+                    thread_id={thread._id}
+                    card_title={thread.title}
+                    id={thread.id}
+                    card_description={thread.description}
+                    flagged={thread.flagged}
+                    no_upvotes={thread.upvotes}
+                    user_name={role === 'professor' ? thread.posted_user : 'anonymous'}
+                    no_replies={thread.replies.length}
+                    replies={thread.replies}
+                    selectedCourse={selectedCourse}
+                />
+            ))}
+            {!loading && selectedCourseThreads.length == 0 && <div className="d-flex mt-5 align-items-center justify-content-center">No threads here Click on the add button to add a thread</div>}
+            <AddNewThread selectedCourse={selectedCourse} selectedCourseThreads={selectedCourseThreads}/>
             {showAlert && (
                 <div className="alert alert-info alert-dismissible fade show" role="alert" style={{ position: 'fixed', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}>
                     Logged in as {role} {alertemail}
                     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={() => setShowAlert(false)}></button>
                 </div>
             )}
+            
         </>
     );
 }
